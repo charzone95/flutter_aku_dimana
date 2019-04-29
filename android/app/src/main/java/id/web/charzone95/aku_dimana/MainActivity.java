d9ec8ebb9c5b1eaa6c9b650459d204592acea941
Charlie Chen <charzone95@gmail.com>
2019-04-29 15:57:10 +0700

    initial commit

---
+++ android/app/src/main/java/id/web/charzone95/aku_dimana/MainActivity.java
@@ -0,0 +1,13 @@
+package id.web.charzone95.aku_dimana;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
